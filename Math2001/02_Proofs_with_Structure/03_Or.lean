39af7b670f784ddd0bd24b9670d5574ded54b313
Subhrajyoty Roy <subhrajyotyroy@gmail.com>
2024-09-02 15:46:49 +0000

    feat: chapter 2

--- Math2001/02_Proofs_with_Structure/03_Or.lean
+++ Math2001/02_Proofs_with_Structure/03_Or.lean
@@ -110,37 +110,168 @@ example {x : ℝ} (h : x = 1 ∨ x = 2) : x ^ 2 - 3 * x + 2 = 0 := by
   }
 
 example {t : ℚ} (h : t = -2 ∨ t = 3) : t ^ 2 - t - 6 = 0 := by
-  sorry
+  obtain h1 | h1 := h
+  {
+    calc
+      t^2 - t - 6 = (-2)^2 - (-2) - 6 := by rw[h1]
+      _ = 0 := by ring
+  }
+  {
+    calc
+      t^2 - t - 6 = 3^2 - 3 - 6 := by rw[h1]
+      _ = 0 := by ring
+  }
 
 example {x y : ℝ} (h : x = 2 ∨ y = -2) : x * y + 2 * x = 2 * y + 4 := by
-  sorry
+  obtain h1 | h1 := h
+  {
+    calc
+      x * y + 2 * x = 2 * y + 2 * 2 := by rw[h1]
+      _ = 2 * y + 4 := by ring
+  }
+  {
+    calc
+      x * y + 2 * x = x * (-2) + 2 * x := by rw[h1]
+      _ = 0 := by ring
+      _ = 2 * (-2) + 4 := by ring
+      _ = 2 * y + 4 := by rw[h1]
+  }
 
 example {s t : ℚ} (h : s = 3 - t) : s + t = 3 ∨ s + t = 5 := by
-  sorry
+  left
+  {
+    calc s + t = 3 := by addarith[h]
+  }
 
 example {a b : ℚ} (h : a + 2 * b < 0) : b < a / 2 ∨ b < - a / 2 := by
-  sorry
+  right
+  {
+    calc
+      b = ((a + 2 * b) - a) / 2 := by ring
+      _ < (0 - a)/2 := by rel[h]
+      _ = -a / 2 := by ring
+  }
 
 example {x y : ℝ} (h : y = 2 * x + 1) : x < y / 2 ∨ x > y / 2 := by
-  sorry
+  left
+  {
+    calc
+      x = (2 * x + 1)/2 - 1/2 := by ring
+      _ < y/2 := by addarith[h]
+  }
 
 example {x : ℝ} (hx : x ^ 2 + 2 * x - 3 = 0) : x = -3 ∨ x = 1 := by
-  sorry
+  have h1 :=
+    calc
+      (x + 3) * (x - 1) = x^2 + 2 * x - 3 := by ring
+      _ = 0 := by rw[hx]
+  have h2 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  obtain h3 | h3 := h2
+  {
+    left
+    calc x = -3 := by addarith[h3]
+  }
+  {
+    right
+    calc x = 1 := by addarith[h3]
+  }
 
 example {a b : ℝ} (hab : a ^ 2 + 2 * b ^ 2 = 3 * a * b) : a = b ∨ a = 2 * b := by
-  sorry
+  have h1 :=
+    calc
+      (a - b) * (a - 2 * b) = a^2 + 2 * b^2 - 3 * a * b := by ring
+      _ = 3 * a * b - 3 * a * b := by rw[hab]
+      _ = 0 := by ring
+  have h2 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  obtain h3 | h3 := h2
+  {
+    left
+    calc a = b := by addarith[h3]
+  }
+  {
+    right
+    calc a = 2 * b := by addarith[h3]
+  }
 
 example {t : ℝ} (ht : t ^ 3 = t ^ 2) : t = 1 ∨ t = 0 := by
-  sorry
+  have h1 :=
+    calc
+      t ^ 2 * (t - 1) = t^3 - t^2 := by ring
+      _ = 0 := by addarith[ht]
+  have h2 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  obtain h3 | h3 := h2
+  {
+    right
+    cancel 2 at h3
+  }
+  {
+    left
+    calc t = 1 := by addarith[h3]
+  }
+
+
 
 example {n : ℕ} : n ^ 2 ≠ 7 := by
-  sorry
+  have h := le_or_succ_le n 2
+  obtain h1 | h1 := h
+  {
+    apply ne_of_lt
+    calc
+      n ^ 2 ≤ 2 ^ 2 := by rel[h1]
+      _ < 7 := by numbers
+  }
+  {
+    apply ne_of_gt
+    calc
+      n ^ 2 ≥ 3^2 := by rel[h1]
+      _ > 7 := by numbers
+  }
 
 example {x : ℤ} : 2 * x ≠ 3 := by
-  sorry
+  have h := le_or_succ_le x 1
+  obtain h1 | h1 := h
+  {
+    apply ne_of_lt
+    calc
+      2 * x ≤ 2 * 1 := by rel[h1]
+      _ < 3 := by numbers
+  }
+  {
+    apply ne_of_gt
+    calc
+      2 * x ≥ 2 * 2 := by rel[h1]
+      _ > 3 := by numbers
+  }
 
 example {t : ℤ} : 5 * t ≠ 18 := by
-  sorry
+  have h := le_or_succ_le t 3
+  obtain h1 | h1 := h
+  {
+    apply ne_of_lt
+    calc
+      5 * t  ≤  5 * 3 := by rel[h1]
+      _ < 18 := by numbers
+  }
+  {
+    apply ne_of_gt
+    calc
+      5 * t ≥ 5 * 4 := by rel[h1]
+      _ > 18 := by numbers
+  }
+
 
 example {m : ℕ} : m ^ 2 + 4 * m ≠ 46 := by
-  sorry
+  have h := le_or_succ_le m 5
+  obtain h1 | h1 := h
+  {
+    apply ne_of_lt
+    calc
+      m^2 + 4 * m ≤ 5^2 + 4 * 5 := by rel[h1]
+      _ < 46 := by numbers
+  }
+  {
+    apply ne_of_gt
+    calc
+      m^2 + 4 * m ≥ 6^2 + 4 * 6 := by rel[h1]
+      _ > 46 := by numbers
+  }
--- Math2001/02_Proofs_with_Structure/04_And.lean
+++ Math2001/02_Proofs_with_Structure/04_And.lean
@@ -20,7 +20,10 @@ example {p : ℚ} (hp : p ^ 2 ≤ 8) : p ≥ -5 := by
       p ^ 2 ≤ 9 := by addarith [hp]
       _ = 3 ^ 2 := by numbers
     numbers
-  sorry
+  obtain ⟨h1, h2⟩ := hp'
+  calc
+    p ≥ -3 := by rel[h1]
+    _ ≥ -5 := by numbers
 
 example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) : a = 9 ∧ b = 1 := by
   constructor
@@ -49,29 +52,113 @@ example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a = 0 ∧ b = 0 := by
       a ^ 2 ≤ a ^ 2 + b ^ 2 := by extra
       _ = 0 := by rw [h1]
     extra
-  sorry
+  have h3 : b^2 = 0 := by addarith[h1, h2]
+  constructor
+  {
+    cancel 2 at h2
+  }
+  {
+    cancel 2 at h3
+  }
 
 /-! # Exercises -/
 
 
 example {a b : ℚ} (H : a ≤ 1 ∧ a + b ≤ 3) : 2 * a + b ≤ 4 := by
-  sorry
+  obtain ⟨h1, h2⟩ := H
+  calc
+    2 * a + b = (a + b) + a := by ring
+    _ ≤ 3 + 1 := by rel[h1, h2]
+    _ = 4 := by ring
 
 example {r s : ℝ} (H : r + s ≤ 1 ∧ r - s ≤ 5) : 2 * r ≤ 6 := by
-  sorry
+  obtain ⟨h1, h2⟩ := H
+  calc
+    2 * r = (r + s) + (r - s) := by ring
+    _ ≤ 1 + 5 := by rel[h1, h2]
+    _ = 6 := by ring
 
 example {m n : ℤ} (H : n ≤ 8 ∧ m + 5 ≤ n) : m ≤ 3 := by
-  sorry
+  obtain ⟨h1, h2⟩ := H
+  have h3 : m + 5 ≤ 8 :=
+    calc
+      m + 5 ≤ n := by rel[h2]
+      _ ≤ 8 := by rel[h1]
+  addarith[h3]
+
 
 example {p : ℤ} (hp : p + 2 ≥ 9) : p ^ 2 ≥ 49 ∧ 7 ≤ p := by
-  sorry
+  have h1 : p ≥ 7 := by addarith[hp]
+  constructor
+  {
+    calc
+      p^2 ≥ 7^2 := by rel[h1]
+      _ = 49 := by ring
+  }
+  {
+    apply h1
+  }
 
 example {a : ℚ} (h : a - 1 ≥ 5) : a ≥ 6 ∧ 3 * a ≥ 10 := by
-  sorry
+  have h1 : a ≥ 6 := by addarith[h]
+  constructor
+  {
+    apply h1
+  }
+  {
+    calc
+      3 * a ≥ 3 * 6 := by rel[h1]
+      _ ≥ 10 := by numbers
+  }
 
 example {x y : ℚ} (h : x + y = 5 ∧ x + 2 * y = 7) : x = 3 ∧ y = 2 := by
-  sorry
+  obtain ⟨h1, h2⟩ := h
+  have h3 : x = 3 :=
+    calc
+      x = 2 * (x + y)  - (x + 2 * y) := by ring
+      _ = 2 * 5 - 7 := by rw[h1, h2]
+      _ = 3 := by ring
+  constructor
+  {
+    apply h3
+  }
+  {
+    addarith[h1, h3]
+  }
 
 example {a b : ℝ} (h1 : a * b = a) (h2 : a * b = b) :
     a = 0 ∧ b = 0 ∨ a = 1 ∧ b = 1 := by
-  sorry
+    have h3 :=
+      calc
+        a * (b - 1) = a * b - a := by ring
+        _ = 0 := by addarith[h1]
+    have h4 := eq_zero_or_eq_zero_of_mul_eq_zero h3
+    obtain h5 | h5 := h4
+    {
+      left
+      constructor
+      {
+        apply h5
+      }
+      {
+        calc
+          b = a * b := by rw[h2]
+          _ = 0 * b := by rw[h5]
+          _ = 0 := by ring
+      }
+    }
+    {
+      right
+      have h6 : b = 1 := by addarith[h5]
+      constructor
+      {
+        calc
+          a = a * 1 := by ring
+          _ = a * b := by rw[h6]
+          _ = b := by rw[h2]
+          _ = 1 := by rw[h6]
+      }
+      {
+        apply h6
+      }
+    }
